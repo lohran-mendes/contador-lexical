e46a5393abafd7eb94b9802fd4c1d1494fa4ee8a
Lohran Mendes <lohranfm@gmail.com>
2025-02-25 17:56:02 -0300

    feat: add logic that write file of text result

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import fs from "node:fs";
 
+const link = process.argv[2];
+
 function receiveFileText() {
-  const link = process.argv[2];
   fs.readFile(link, "utf-8", (error, data) => {
     try {
       if (error) throw error;
-      console.log(
-        separateParagraphsInObjects(separateTextIntoParagraphs(data))
+      writeReturnedFiles(
+        JSON.stringify(
+          separateParagraphsInObjects(separateTextIntoParagraphs(data))
+        )
       );
     } catch (error) {
       if (error.code === "ENOENT") {
@@ -43,15 +46,14 @@ function separateParagraphsInObjects(paragraphs) {
 
 function separateRepeatedWords(paragraph) {
   if (typeof paragraph === "string" && paragraph.length > 0) {
-    const regex = /[^\w\sáéíóúãõâêîôûàèìòù]/g;
+    const regex = /[^\w\sáéíóúãõâêîôûàèìòùç]/g;
     let arrayWord = paragraph.toLowerCase().replace(regex, "").split(" ");
-    let objectOfRepeatedWords = arrayWord.reduce(
-      (previousValue, currentValue) => {
+    let objectOfRepeatedWords = arrayWord
+      .filter((word) => word)
+      .reduce((previousValue, currentValue) => {
         previousValue[currentValue] = (previousValue[currentValue] || 0) + 1;
         return previousValue;
-      },
-      {}
-    );
+      }, {});
     for (const key in objectOfRepeatedWords) {
       if (objectOfRepeatedWords[key] === 1) {
         delete objectOfRepeatedWords[key];
@@ -59,6 +61,31 @@ function separateRepeatedWords(paragraph) {
     }
     return objectOfRepeatedWords;
   }
+}
+
+function writeReturnedFiles(data) {
+  const nameFile = link.slice(link.lastIndexOf("/") + 1);
+  fs.writeFile(`./returned-files/${nameFile}`, treatTextData(data), (error) => {
+    try {
+      if (error) throw error;
+      console.log(
+        `\nPalavras duplicadas de ${nameFile} salvo com sucesso em returned-files!\n`
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  });
+}
+
+function treatTextData(data) {
+  const enteredJson = JSON.parse(data);
+  let textReturned = "";
+  for (const element of enteredJson) {
+    textReturned += `Palavras duplicadas no parágrafo ${
+      element.paragraph
+    }: ${Object.keys(element.repeatedWords).join(", ")}\n`;
+  }
+  return textReturned;
 }
 
 receiveFileText();
